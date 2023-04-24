a7627ae0832abc9b3d89839ebacfe613d867dff9
alvesadailton <adailtonb008@gmail.com>
2023-04-24 16:12:21 -0300

    commitado

---
+++ assets/js/script.js
@@ -0,0 +1,128 @@
+{
+	const characters = {
+		'star-lord': {
+			title: 'Star-Lord<br>(Senhor das Estrelas)',
+			img: 'https://cdn.marvel.com/u/prod/marvel/i/mg/9/a0/582f39a4ddd43/clean.jpg',
+			description: 'Peter Quill, também conhecido como Senhor das Estrelas (Star-Lord no original), é um super-herói que aparece nos quadrinhos americanos publicados pela Marvel Comics. O personagem, criado por Steve Englehart e Steve Gan, apareceu pela primeira vez na Marvel Preview # 4 (janeiro de 1976). Quill é filho da humana Meredith Quill e do Spartoi J\'son, Peter Quill assume o manto de Senhor das Estrelas, um policial interplanetário. O personagem desempenhou papéis proeminentes nos arcos de história "Annihilation" (2006) e "Annihilation: Conquest" (2007), "War of Kings" (2008) e The Thanos Imperative (2009). Ele se tornou o líder da equipe de super-heróis espacial Guardiões da Galáxia no relançamento de 2008 da equipe. Ele foi apresentado em uma variedade de produtos da Marvel, incluindo séries animadas de televisão, brinquedos entre outros produtos. O ator Chris Pratt interpreta Peter Quill no filme live-action Guardiões da Galáxia e sua sequência Guardiões da Galáxia Vol. 2. Ele retoma o papel em Vingadores: Guerra Infinita e na sua sequência Vingadores: Ultimato.'
+		},
+		gamora: {
+			title: 'Gamora',
+			img: 'https://www.einerd.com.br/wp-content/uploads/2021/10/what-if-gamora.jpg',
+			description: 'Gamora foi uma personagem fictícia que apareceu nas histórias em quadrinhos publicadas pela Marvel Comics. Criada por Jim Starlin, a personagem apareceu pela primeira vez em "Strange Tales" vol. 180 (Junho de 1975). Gamora foi a filha adotiva de Thanos, e a última de sua espécie. Ela já apareceu como o interesse amoroso de Adam Warlock e Nova. Também já foi membro do grupo conhecido como Guarda do Infinito e hoje integra o grupo Guardiões da Galáxia. Ela desempenhou um importante papel nos eventos da saga "Aniquilação: A Conquista" (2007) e tornou-se um membro titular da equipe Guardiões da Galáxia. Ela tem sido destaque em uma variedade de mercadoria da Marvel. É interpretada por Zoë Saldaña nos filmes Guardiões da Galáxia e Guardiões da Galáxia Vol. 2.',
+		},
+		kang: {
+			title: 'Kang, o Conquistador',
+			img: 'https://naoseinadadotcomdotbr.files.wordpress.com/2021/11/0027c-kang-o-conquistador-naoseinada.jpg',
+			description: 'Kang, o Conquistador (Kang the Conqueror) é um dos personagens do Universo Marvel. Suposto descendente de Nathaniel Richards, pai de Reed Richards, ele também pode ser considerado uma das contrapartes futuras do líder do Quarteto Fantástico, embora haja especulações de que na verdade ele seja parente do Doutor Destino, como sugerido em uma de suas primeiras histórias. Inimigo dos Vingadores e do Quarteto Fantástico, ele também é um déspota temporal. Sua versão jovem se tornou o Rapaz de Ferro, dos Jovens Vingadores.',
+		},
+		ironheart: {
+			title: 'Ironheart<br>Riri Williams',
+			img: 'https://www.hqzona.com.br/wp-content/uploads/2019/02/clean-800x445.jpg',
+			description: 'Riri Williams ou Coração de Ferro (Ironheart no original) é uma personagem fictícia dos quadrinhos americanos publicados pela Marvel Comics. Riri é uma estudante de 15 anos que ingressou precocemente no MIT, a mais prestigiada faculdade de tecnologia dos Estados Unidos. Com uma inteligencia acima do normal, ela constrói sua própria versão da armadura do Homem de Ferro, utilizando materiais que ela furta pelo campus. A personagem foi introduzida em Março de 2016, em Invincible Iron Man #7. Trabalhando sozinha, Riri projeta uma armadura semelhante à armadura do Homem de Ferro usando material roubado do campus. Quando o segurança do campus bate em sua porta, ela foge enquanto veste a armadura. Quando Williams impede que dois presos escapem da Penitenciária do Estado do Novo México, sua armadura fica danificada. Ao voltar para a casa de sua mãe, Riri continuou a trabalhar na melhoria do processo, para desgosto de sua tia. Tony Stark ouve as realizações de Riri e vai encontrá-la. Durante a reunião, Tony Stark decide que ele endossará sua decisão de se tornar uma super-heroína.',
+		},
+		maquina: {
+			title: 'Máquina de Combate',
+			img: 'https://www.einerd.com.br/wp-content/uploads/2019/11/maquina-de-combate-vingadores-ultimato-e1574429352213.jpg',
+			description: 'James Rhodes era um bom garoto nascido e criado na Filadélfia, Ao crescer quis se unir as forças militares e servir como piloto. Em uma missão de guerra, o seu helicóptero foi abatido. Quando tentava obter um avião encontrou Tony Stark em sua primeira armadura de Homem de Ferro que tinha acabado de escapar do cárcere inimigo. Juntos, Rhodes e Stark conseguem retornar aos EUA. Tony oferece emprego a Jim, mas ele recusa pois ainda está interessado pelo trabalho de defender os EUA nos combates. Mal sabia ele o que o destino o reservava. Após a Guerra, Stark, que tinha secretamente se tornado o super-herói, ofereceu a Rhodes um emprego como seu motorista pessoal. Rhodes logo se tornou um dos colaboradores mais próximos de Stark e conquistou sua confiança bem como o cargo de tesoureiro das Indústrias Stark. Como resultado, ele teve inúmeras aventuras com seu empregador onde a sua ousadia e competências eram qualidades valiosas. Stark era tão confidente de Rhodes que ele sabia que era o Homem de Ferro. Quando Stark perdeu sua empresa para Obadiah Stane e caiu no alcoolismo, Rhodes assumiu o papel de Homem de Ferro, ajudando neste ínterim Stark a vencer o alcoolismo.',
+		},
+		cassie: {
+			title: 'Cassie Lang',
+			img: 'https://www.themarysue.com/wp-content/uploads/2023/02/cassie-lang-ant-man-marvel.jpg',
+			description: 'Cassandra "Cassie" Lang é uma personagem que aparece nos quadrinhos americanos publicados pela Marvel Comics. Criado por David Michelinie e John Byrne, o personagem apareceu pela primeira vez em Marvel Premiere #47 (abril de 1979). Cassandra Lang é filha do super-herói Scott Lang. A personagem também é conhecida como Stature, Stinger e Ant-Girl em vários pontos de sua história. No Universo Cinematográfico Marvel (MCU), a personagem foi retratada quando criança por Abby Ryder Fortson em Homem-Formiga (2015) e Homem-Formiga e a Vespa (2018), e como adolescente por Emma Fuhrmann em Vingadores: Ultimato ( 2019) e Kathryn Newton em Homem-Formiga e a Vespa: Quantumania (2023).',
+		},
+		yelena: {
+			title: 'Yelena Belova',
+			img: 'https://www.hqzona.com.br/wp-content/uploads/2021/07/433ybv_com_mas_mob_03.jpg',
+			description: 'Yelena Belova (Ucraniano: Олена Бєлова, Russo: Елена Белова) é uma personagem das histórias em quadrinhos do Universo Marvel, publicado pela Marvel Comics. A segunda a usar o nome Viúva Negra na Era Moderna. Criada por Devin Grayson (roteiro) e J.G. Jones (desenhos), ela apareceu pela primeira vez em Inhumans #5 (Março de 1999). Yelena foi treinada como espiã e assassina na Sala Vermelha. Originalmente era inimiga de Natasha Romanoff e foi enviada para matá-la, porém mais tarde acabam se tornando aliadas. Ela também foi membro da S.H.I.E.L.D., Vanguarda e HYDRA, que a transformou em uma versão do Super-Adaptoid. Como Super-Adaptoid, ela chegou ao Alto Conselho da I.M.A. Voltou a usar o seu codinome original, Viúva Negra, em 2017.',
+		},
+		monica: {
+			title: 'Monica Rambeau',
+			img: 'https://observatoriodocinema.uol.com.br/wp-content/uploads/2019/04/marvel-legends-monica-rambeau-toyrus-2017-capit-marvel-D_NQ_NP_819991-MLB26883164316_022018-F.jpg',
+			description: 'Monica Rambeau é uma super-heroína de histórias em quadrinhos americanas da Marvel Comics. Ela já foi um membro dos Vingadores, utilizando os codinomes Capitã Marvel, Pulsar e Fóton. Atualmente é conhecida como Espectro. Capitã Marvel foi criada por Roger Stern e John Romita, Jr. e sua primeira aventura foi em Amazing Spider-Man Annual #16 (1982). Em sua identidade civil de Monica Rambeau, foi notada a semelhança com Pam Grier, depois acentuada pelo ilustrador John Romita Jr.',
+		},
+	};
+
+	// eslint-disable-next-line no-inner-declarations
+	function main() {
+		const modal = document.querySelector('.allJS');
+		const cardsElements = document.querySelectorAll('[data-character]');
+
+		const backgroundBlur = document.createElement('div');
+		const containerDiv = document.createElement('div');
+
+		backgroundBlur.setAttribute('class', 'js');
+		containerDiv.setAttribute('class', 'js-div');
+
+		function createWindow() {
+			modal.appendChild(backgroundBlur);
+			backgroundBlur.appendChild(containerDiv);
+		}
+
+		const sectionTitle = document.createElement('section');
+		const title = document.createElement('h1');
+		sectionTitle.setAttribute('class', 't-section');
+		title.setAttribute('class', 'title');
+
+		const sectionImg = document.createElement('section');
+		const img = document.createElement('img');
+		sectionImg.setAttribute('class', 'i-section');
+		img.setAttribute('class', 'img-container');
+
+		const sectionDescription = document.createElement('section');
+		const description = document.createElement('p');
+		sectionDescription.setAttribute('class', 'd-section');
+		description.setAttribute('class', 'description');
+
+		function createContainer() {
+			containerDiv.appendChild(sectionTitle);
+			containerDiv.appendChild(sectionImg);
+			containerDiv.appendChild(sectionDescription);
+		}
+
+		function setContainer() {
+			sectionTitle.appendChild(title);
+			sectionImg.appendChild(img);
+			sectionDescription.appendChild(description);
+		}
+
+		cardsElements.forEach(element => element.addEventListener('click', () => {
+			const character = element.dataset.character;
+
+			createWindow();
+			closeButton();
+			createContainer();
+			setContainer();
+
+			title.innerHTML = characters[character].title;
+			img.src = characters[character].img;
+			description.innerHTML = characters[character].description;
+		}));
+
+		function closeModalWhenKeydownEscape(e) {
+			e.key === 'Escape' && removeModalChildren();
+		}
+
+		function removeModalChildren() {
+			const modal = document.querySelector('.allJS');
+			modal.firstChild && modal.removeChild(modal.firstChild);
+
+			window.removeEventListener('keydown', closeModalWhenKeydownEscape);
+		}
+
+		const close = document.createElement('div');
+		close.setAttribute('class', 'close');
+
+		function closeButton() {
+			containerDiv.appendChild(close);
+
+			window.addEventListener('keydown', closeModalWhenKeydownEscape);
+
+			close.addEventListener('click', e => {
+				const el = e.target;
+				el.classList.contains('close') && removeModalChildren();
+			});
+		}
+	}
+
+	main();
+}
